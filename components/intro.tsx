6dbb999b3ee17f754f4fb571287d447262132dff
Hardaat Singh Baath <hardaatbaath11@gmail.com>
2023-12-12 19:44:39 +0530

    Update intro.tsx

--- components/intro.tsx
+++ components/intro.tsx
@@ -63,10 +63,10 @@ export default function Intro() {
         animate={{ opacity: 1, y: 0 }}
       >
         <span className="font-bold">Hello, I'm Ricardo.</span> I'm a{" "}
-        <span className="font-bold">full-stack developer</span> with{" "}
-        <span className="font-bold">8 years</span> of experience. I enjoy
+        <span className="font-bold">ML Engineer</span> with{" "}
+        <span className="font-bold">2 years</span> of experience. I enjoy
         building <span className="italic">sites & apps</span>. My focus is{" "}
-        <span className="underline">React (Next.js)</span>.
+        <span className="underline">Dl and Robotics</span>.
       </motion.h1>
 
       <motion.div
