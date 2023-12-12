77b2753fafe5300a763368599e8dfe043047788e
Hardaat Singh Baath <hardaatbaath11@gmail.com>
2023-12-12 20:08:01 +0530

    Update intro.tsx

--- components/intro.tsx
+++ components/intro.tsx
@@ -31,7 +31,7 @@ export default function Intro() {
             }}
           >
             <Image
-              src="https://github.com/hardaatbaath/hardaatbaath/blob/main/images/hardaat_formal%20copy.jpeg"
+              src="https://github.com/hardaatbaath/portfolio-website/blob/master/public/hardaat_formal%20copy.jpeg"
               alt="Hardaat's portrait"
               width="192"
               height="192"
