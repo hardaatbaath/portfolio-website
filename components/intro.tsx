f9b24c76731f4b8aa9938d68c343a9757ec9ee7e
Hardaat Singh Baath <hardaatbaath11@gmail.com>
2023-12-12 19:49:53 +0530

    Update intro.tsx

--- components/intro.tsx
+++ components/intro.tsx
@@ -62,11 +62,11 @@ export default function Intro() {
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
       >
-        <span className="font-bold">Hello, I'm Ricardo.</span> I'm a{" "}
+        <span className="font-bold">Hello, I'm Hardaat Singh Baath.</span> I'm an{" "}
         <span className="font-bold">ML Engineer</span> with{" "}
-        <span className="font-bold">2 years</span> of experience. I enjoy
-        building <span className="italic">sites & apps</span>. My focus is{" "}
-        <span className="underline">Dl and Robotics</span>.
+        <span className="font-bold">1 year</span> of experience. I enjoy
+        solving challenges using<span className="italic"> AI models</span>. My focus is{" "}
+        <span className="underline">DL and Robotics</span>.
       </motion.h1>
 
       <motion.div
@@ -100,7 +100,7 @@ export default function Intro() {
 
         <a
           className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-          href="https://linkedin.com"
+          href="https://linkedin.com/in/hardaat-singh-baath"
           target="_blank"
         >
           <BsLinkedin />
@@ -108,7 +108,7 @@ export default function Intro() {
 
         <a
           className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-          href="https://github.com"
+          href="https://github.com/hardaatbaath"
           target="_blank"
         >
           <FaGithubSquare />
