98fbda8e9567ecdf5ec5d118870e3cd140372fc5
Hardaat Singh Baath <hardaatbaath11@gmail.com>
2023-12-17 12:28:39 +0530

    Update data.ts

--- lib/data.ts
+++ lib/data.ts
@@ -2,6 +2,7 @@ import React from "react";
 import { CgWorkAlt } from "react-icons/cg";
 import { FaReact } from "react-icons/fa";
 import { LuGraduationCap } from "react-icons/lu";
+import kratos_descriptionImg from "@/public/kratos_description.png";
 import corpcommentImg from "@/public/corpcomment.png";
 import rmtdevImg from "@/public/rmtdev.png";
 import wordanalyticsImg from "@/public/wordanalytics.png";
