4ef075271161d75c926621b898890495e08e4118
Hardaat Singh Baath <hardaatbaath11@gmail.com>
2023-12-18 21:43:09 +0530

    Update about.tsx

--- components/about.tsx
+++ components/about.tsx
@@ -20,20 +20,19 @@ export default function About() {
       <SectionHeading>About me</SectionHeading>
       <p className="mb-3">
         I am currently pursuing a bachelor's degree in{" "}
-        <span className="font-medium">Computer Science and Information Systems</span> from BITS Pilani, Goa. I have a passion 
-        for autonomous robotics and programming. I believe in project-based learning and have completed various guided 
-        projects in Coursera in the field of {" "} <span className="font-medium">Machine Learning and Deep Learning</span>.{" "}
-        <span className="italic">My favourite part of programming is</span> the 
+        <span className="font-medium">Computer Science</span> from BITS Pilani, Goa. I have a passion 
+        for autonomous robotics and Deep Learning and have completed various
+        projects in the field of {" "} <span className="font-medium">ML and DL.</span>.{" "}
+        <span className="italic"> My favourite part of programming is</span> the 
         problem-solving aspect. I <span className="underline">love</span> the
         feeling of finally figuring out a solution to a problem after trying to solve it forever. My core stack
         is{" "}
         <span className="font-medium">
-          C++, Python, Numpy, Matplotlib, Pandas, PyTorch and ROS
+          C++ and Python
         </span>
         . I am also familiar with C, Java and TensorFlow. I am always looking to
         learn new technologies. I am currently looking for {" "}
-        <span className="font-medium"> internship opportunities</span> as a research intern or software
-        developer.
+        <span className="font-medium"> internship opportunities</span> as a research intern or ML Engineer.
       </p>
 
       <p>
